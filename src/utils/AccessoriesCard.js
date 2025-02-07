4e5e307c8e70a63d0ce47201842fe1f3e5366237
Developer <developer@MacBook-Pro-Dan.local>
2025-02-07 15:09:34 +0200

    first commit

---
+++ src/utils/AccessoriesCard.js
@@ -0,0 +1,14 @@
+
+const AccesoriesCard = ({img,alt,title,link}) => {
+    return (
+        <article>
+            <a href="">
+                
+            </a>
+            <img src={img} alt={alt}/>
+            <h4>{title}</h4>
+            <button>More</button>
+        </article>
+    )
+}
+export default AccesoriesCard
