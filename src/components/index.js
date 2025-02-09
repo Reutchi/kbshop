ba6e10366b3ec1ad0419c55829b0d59d4219d5eb
Developer <developer@MacBook-Pro-Dan.local>
2025-02-09 19:46:16 +0200

    reutilisable section component

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,14 @@ import SocialEnjoy from "@/components/SocialEnjoy";
 import ProductSection from "@/components/ProductSection";
 import NewArrivalSection from "@/components/NewArrivalSection";
 import AccessoriesSection from "@/components/AccessoriesSection";
+import SectionReutilisable from "@/utils/SectionReutilisable";
+import PBtfansSection from "@/components/PBtfansSection";
 export {
     Navigation,
     SocialEnjoy,
     ProductSection,
     NewArrivalSection,
     AccessoriesSection,
+    SectionReutilisable,
+    PBtfansSection,
 }
