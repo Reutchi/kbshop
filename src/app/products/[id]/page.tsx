d1ef99a4c6596fe93f59c7c415fe20d68f74be0c
Reutchi <reutchi1997@icloud.com>
2025-02-11 23:26:26 +0200

    test

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,22 +2,13 @@ import { notFound } from 'next/navigation';
 import { FooterSection, Navigation, ProductDetailsCard } from '../../../components';
 import React from 'react';
 
-interface ProductPageProps {
-    params: { id: string };
-}
-
-export async function generateMetadata({ params }: ProductPageProps) {
-    // Poți adăuga metadate dacă ai nevoie
-    return { title: `Product ${params.id}` };
-}
-
-const ProductPage = ({ params }: ProductPageProps) => {
-    if (!params.id) {
-        return notFound();
-    }
+type paramsProps = { params: string };
+const ProductPage: React.FC = async ({ params }: paramsProps) => {
+    const { id } = await params;
 
     return (
         <div className="flex flex-col min-h-screen">
+            <p>ID: {id}</p>
             <Navigation />
             <main className="flex-1 flex justify-center items-center">
                 <ProductDetailsCard />
