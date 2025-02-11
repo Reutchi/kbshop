e048759512de790d16b4ceb374913f900b0e1e4e
Reutchi <reutchi1997@icloud.com>
2025-02-11 23:34:47 +0200

    bug

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,10 +1,10 @@
 import { notFound } from 'next/navigation';
 import { FooterSection, Navigation, ProductDetailsCard } from '../../../components';
-import React from 'react';
 
-type paramsProps = { params: { id: string } };
+type paramsProps = { params: any }; // Folosind any pentru params
+
 const ProductPage = async ({ params }: paramsProps) => {
-    const { id } = await params;
+    const { id } = await params; // id va fi tipizat ca any
 
     return (
         <div className="flex flex-col min-h-screen">
